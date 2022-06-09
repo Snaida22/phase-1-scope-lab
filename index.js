f3274bb60c9a2a0aa76fff8740bbd13e3e044e22
Snaida.Kabalika <snaida.kabalika@student.moringaschool.com>
2022-06-09 20:44:36 +0300

    commit tree 1e2869cc00fb6acd60ebf72fc839592e1b0ee060

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = "bob"
+var bestCustomer;
+const LeastFavoriteCustomer ="maybe not bob"
+console.log(customerName)
+
+function upperCaseCustomerName(){
+    customerName= customerName.toLocaleUpperCase();
+    return customerName;
+}
+
+function setBestCustomer(){
+    bestCustomer="not bob"
+    return bestCustomer;
+ }
+
+ function overwriteBestCustomer(){
+    bestCustomer ="maybe bob"
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer(){
+    LeastFavoriteCustomer="its bob now"
+    return changeLeastFavoriteCustomer;
+}
